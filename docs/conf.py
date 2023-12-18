fa2224af88f4dd8cab4cf716ce837e91063954e0
ATreeShine <66901031+ATreeShine@users.noreply.github.com>
2023-12-18 14:38:57 +0600

    Update conf.py
    I added some comments to give proper understandings please accept it :)

--- docs/conf.py
+++ docs/conf.py
@@ -1,42 +1,36 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Importing necessary modules
 import os
 import sys
 
-# Code paths for documentation
+# Paths to the code for which the documentation is to be generated
 code_paths = ['../deadline/plugins/UnrealEngine',
               '../unreal/UnrealDeadlineService/Content/Python',
               '../unreal/MoviePipelineDeadline/Content/Python']
 
+# Adding the code paths to the system path
 for path in code_paths:
     path = os.path.abspath(path)
     print(path)
     if os.path.exists(path):
         sys.path.insert(0, path)
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# Project information
+project = 'Unreal Engine Deadline'  # The name of the project
+copyright = '2023, Epic Games Inc'  # Copyright information
+author = 'Art Pipeline'  # The author of the project
 
-project = 'Unreal Engine Deadline'
-copyright = '2023, Epic Games Inc'
-author = 'Art Pipeline'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# Extensions for Sphinx to use when building the documentation
 extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
-# Autodoc settings
+# Mock imports for autodoc to prevent import errors
 autodoc_mock_imports = ['unreal', 'deadline_shared', 'Deadline', 'FranticX']
 
+# Path to the templates
 templates_path = ['_templates']
+# Patterns to exclude from the documentation
 exclude_patterns = []
 
-
-# -- Options for HTML html -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# Theme for the HTML documentation
 html_theme = 'sphinx_rtd_theme'
+# Path to static files for the HTML documentation
 html_static_path = ['_static']
